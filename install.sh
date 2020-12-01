8e9d5a4ffddf9193642d6625ed76c3fe5dee096b
root <root@akash.us-central1-a.c.first-outlet-269615.internal>
2020-12-01 18:17:06 +0000

    challenge1

---
+++ install.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+wget https://golang.org/dl/go1.15.5.linux-amd64.tar.gz
+tar -C /usr/local -xzf go1.15.5.linux-amd64.tar.gz
+export PATH="$PATH:/usr/local/go/bin"
+export GOPATH="$HOME/go"
+export GOROOT="/usr/local/go"
+export PATH="$GOPATH/bin:$GOROOT/bin:$PATH"
+export AKASH_NET="https://raw.githubusercontent.com/ovrclk/net/master/edgenet"
+export AKASH_VERSION="$(curl -s "$AKASH_NET/version.txt")"
+export AKASH_CHAIN_ID="$(curl -s "$AKASH_NET/chain-id.txt")"
+export AKASH_NODE="$(curl -s "$AKASH_NET/rpc-nodes.txt" | shuf -n 1)"
+
+go get -d http://github.com/ovrclk/akash
+cd $GOPATH/src/github.com/ovrclk/akash
+git checkout "$AKASH_VERSION"
+apt install make
+apt-get install build-essential
+make deps-install
+make install
