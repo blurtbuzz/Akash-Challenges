c60cf100fbc927ee0ac5593d325a59bdcd6f739e
blurtbuzz <75323535+blurtbuzz@users.noreply.github.com>
2020-12-02 16:22:23 -0500

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -2,7 +2,7 @@
 export AKASH_NET="https://raw.githubusercontent.com/ovrclk/net/master/edgenet"
 export AKASH_VERSION="$(curl -s "$AKASH_NET/version.txt")"
 export AKASH_CHAIN_ID="$(curl -s "$AKASH_NET/chain-id.txt")"
-export AKASH_NODE="$(curl -s "$AKASH_NET/rpc-nodes.txt" | shuf -n 1)"
+export AKASH_NODE="tcp://rpc-edgenet.akashdev.net:26657"
 export PATH="$PATH:/usr/local/go/bin"
 export GOPATH="$HOME/go"
 export GOROOT="/usr/local/go"
