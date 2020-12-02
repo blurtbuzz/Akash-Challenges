675ae13fc5d251b125cc5a301c95054f67aca91f
root <root@vmi358717.contaboserver.net>
2020-12-02 09:39:39 +0800

    setup for challenge2

---
+++ setup.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+export AKASH_NET="https://raw.githubusercontent.com/ovrclk/net/master/edgenet"
+export AKASH_VERSION="$(curl -s "$AKASH_NET/version.txt")"
+export AKASH_CHAIN_ID="$(curl -s "$AKASH_NET/chain-id.txt")"
+export AKASH_NODE="$(curl -s "$AKASH_NET/rpc-nodes.txt" | shuf -n 1)"
+export PATH="$PATH:/usr/local/go/bin"
+export GOPATH="$HOME/go"
+export GOROOT="/usr/local/go"
+export PATH="$GOPATH/bin:$GOROOT/bin:$PATH"
